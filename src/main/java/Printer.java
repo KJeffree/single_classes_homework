cf4fe6d757a2557ccd5e1cad025c8c06ace838ba
Katie Jeffree <k.jeffree@gmail.com>
2018-11-19 16:46:27 +0000

    finished printer class

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,32 @@
 public class Printer {
+    private int paperLeft;
+    private int tonerLevel;
+
+    public Printer(int paperLeft, int tonerLevel){
+        this.paperLeft = paperLeft;
+        this.tonerLevel = tonerLevel;
+    }
+
+    public int getPaperLeft() {
+        return paperLeft;
+    }
+
+    public String print(int pagesToPrint, int numberOfCopies) {
+        int numberOfPages = pagesToPrint * numberOfCopies;
+        if (this.paperLeft > numberOfPages){
+            this.paperLeft -= numberOfPages;
+            this.tonerLevel -= numberOfPages;
+            return "Success";
+        } else {
+            return "Error";
+        }
+    }
+
+    public void refill() {
+        this.paperLeft = 50;
+    }
+
+    public int getTonerLevel() {
+        return tonerLevel;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    private Printer printer;
+    @Before
+    public void before(){
+        printer = new Printer(50, 200);
+    }
+
+    @Test
+    public void hasPaperLeft(){
+        assertEquals(50, printer.getPaperLeft());
+    }
+
+    @Test
+    public void canPrint(){
+        assertEquals("Success", printer.print(10, 3));
+        assertEquals(20, printer.getPaperLeft());
+    }
+
+    @Test
+    public void cannotPrintIfNotEnoughPaper(){
+        assertEquals("Error", printer.print(10, 10));
+    }
+
+    @Test
+    public void canRefilPrinter(){
+        printer.print(10, 2);
+        printer.refill();
+        assertEquals(50, printer.getPaperLeft());
+    }
+
+    @Test
+    public void hasTonerLevel(){
+        assertEquals(200, printer.getTonerLevel());
+    }
+
+    @Test
+    public void canUseToner(){
+        printer.print(10, 2);
+        assertEquals(180, printer.getTonerLevel());
+    }
+
 }
